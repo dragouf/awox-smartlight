17aaa0fd372a631f98b6166dcd7c48c5c7a7ed6b
David LAURENT <da.laurent@laposte.net>
2017-10-02 23:32:10 +0200

    + build

--- build/lib.js
+++ build/lib.js
@@ -4,13 +4,17 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var nobleExtended = require('noble-extended');
+var nobleExtended = require('../../noble');
 
-var SEQUENCE_ON = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01, 0x0A, 0x01, 0x01, 0x00, 0x28, 0x0D];
-var SEQUENCE_OFF = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01, 0x0A, 0x01, 0x00, 0x01, 0x28, 0x0D];
-var SEQUENCE_BRIGHNTESS = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01, 0x0C, 0x01, 0x00, 0xEC, 0x15, 0x0D];
-var SEQUENCE_WHITE = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01, 0x0E, 0x01, 0x00, 0x14, 0x3F, 0x0D];
-var SEQUENCE_RGB = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01, 0x0D, 0x06, 0x01, 0x00, 0x00, 0x00, 0x20, 0x30, 0xF8, 0x78, 0x0D];
+var HEADER = [0xAA, 0x0A, 0xFC, 0x3A, 0x86, 0x01];
+var TAIL = [0x0D];
+
+var SEQUENCE_ON = HEADER.concat([0x0A, 0x01, 0x01, 0x00, 0x28], TAIL);
+var SEQUENCE_OFF = HEADER.concat([0x0A, 0x01, 0x00, 0x01, 0x28], TAIL);
+var SEQUENCE_BRIGHNTESS = HEADER.concat([0x0C, 0x01, 0x00, 0xEC]);
+var SEQUENCE_WHITE = HEADER.concat([0x0E, 0x01, 0x00, 0x14]);
+var SEQUENCE_WHITE_RESET = HEADER.concat([0x0D, 0x06, 0x02, 0x20, 0x30, 0x40, 0x50, 0x60]);
+var SEQUENCE_RGB = HEADER.concat([0x0D, 0x06, 0x01, 0x00, 0x00, 0x00, 0x20, 0x30, 0xF8]);
 
 module.exports = function () {
   function AwoxSmartLight(lampMac, logger) {
@@ -69,38 +73,59 @@ module.exports = function () {
   }, {
     key: "lightBrightness",
     value: function lightBrightness(intensity) {
-      SEQUENCE_BRIGHNTESS[8] = intensity;
-      SEQUENCE_BRIGHNTESS[10] = this._checksum(SEQUENCE_BRIGHNTESS);
-      this.commandsQueue.push(SEQUENCE_BRIGHNTESS);
+      // value
+      SEQUENCE_BRIGHNTESS[8] = Math.floor(intensity * 9 + 2);
+      // random
+      SEQUENCE_BRIGHNTESS[9] = Math.floor(Math.random() * 0xFF) >>> 0;
+      // checksum
+      SEQUENCE_BRIGHNTESS.push(this._checksum(SEQUENCE_BRIGHNTESS));
+
+      this.commandsQueue.push(SEQUENCE_BRIGHNTESS.concat(TAIL));
       this.lightCommandDebounce();
     }
   }, {
     key: "lightWhite",
     value: function lightWhite(temperature) {
-      SEQUENCE_WHITE[8] = temperature;
-      SEQUENCE_WHITE[10] = this._checksum(SEQUENCE_WHITE);
-      this.commandsQueue.push(SEQUENCE_WHITE);
+      // value
+      SEQUENCE_WHITE[8] = Math.floor(temperature * 9 + 2);
+      // random
+      SEQUENCE_WHITE[9] = Math.floor(Math.random() * 0xFF) >>> 0;
+      // checksum
+      SEQUENCE_WHITE.push(this._checksum(SEQUENCE_WHITE));
+      this.commandsQueue.push(SEQUENCE_WHITE.concat(TAIL));
+      this.lightCommandDebounce();
+    }
+  }, {
+    key: "lightWhiteReset",
+    value: function lightWhiteReset() {
+      // random
+      SEQUENCE_WHITE_RESET.push(Math.floor(Math.random() * 0xFF) >>> 0);
+      // checksum
+      SEQUENCE_WHITE_RESET.push(this._checksum(SEQUENCE_WHITE_RESET));
+      this.commandsQueue.push(SEQUENCE_WHITE_RESET.concat(TAIL));
       this.lightCommandDebounce();
     }
   }, {
     key: "lightRgb",
     value: function lightRgb(r, g, b, special) {
       SEQUENCE_RGB[8] = special ? 0x02 : 0x01;
+      // RGB values
       SEQUENCE_RGB[9] = r;
       SEQUENCE_RGB[10] = g;
       SEQUENCE_RGB[11] = b;
-      //SEQUENCE_RGB[14] = rand() % 256;
-      SEQUENCE_RGB[15] = this._checksum(SEQUENCE_RGB);
-      this.commandsQueue.push(SEQUENCE_RGB);
+      // random
+      SEQUENCE_RGB[14] = Math.floor(Math.random() * 0xFF) >>> 0;
+
+      SEQUENCE_RGB.push(this._checksum(SEQUENCE_RGB));
+      this.commandsQueue.push(SEQUENCE_RGB.concat(TAIL));
       this.lightCommandDebounce();
     }
   }, {
     key: "_checksum",
-    value: function _checksum(command) {
-      var sum = 0;
-      for (var i = 1; i + 2 < command.length; i++) {
-        sum += command[i];
-      }return sum + 85;
+    value: function _checksum(data) {
+      return data.slice(1).reduce(function (a, b) {
+        return a + b;
+      }) + 85 & 0xFF;
     }
   }, {
     key: "_onPeripheralDiscover",
